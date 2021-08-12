ce3fc91c19566d4d1fb95c4624306c17bcfca302
Maria Khelli <71018093+khelli07@users.noreply.github.com>
2021-08-13 00:20:39 +0700

    add frontend login register

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <h2 style={{ margin: "auto", width: "80%" }}>Hello, this is home page!</h2>
+    <div style={{ margin: "auto", width: "80%" }}>
+      <h2>Hello, this is home page!</h2>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+    </div>
   );
 }
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,8 +3,15 @@ import LoginForm from "../Components/LoginForm";
 
 function Login() {
   return (
-    <div style={{ width: "80%", margin: "auto" }}>
-      <LoginForm />
+    <div
+      style={{
+        background:
+          "linear-gradient(180deg, #ffffff 0%, #effff9 82.29%, #e3fff5 100%)",
+      }}
+    >
+      <div style={{ width: "80%", margin: "auto" }}>
+        <LoginForm />
+      </div>
     </div>
   );
 }

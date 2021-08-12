45ea602ee031846307032796e3ad8bc5e90826cc
hafidznrg <hafidznur.hn@gmail.com>
2021-08-12 21:43:31 +0700

    bikin chatbox

---
+++ src/Components/ChatBox/ChatBox.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styles from './Chat.module.css';
+import { Grid, Link, Paper } from '@material-ui/core';
+import { DataChat } from './DataChat';
+import * as IoIcons from 'react-icons/io';
+
+function Chat() {
+    const [pesan, setPesan] = useState('');
+
+    const submitPesan = () => {
+        if (pesan === ''){
+            console.log('Pesannya kosong nih');
+        } else {
+            const message = {
+                sent : true,
+                body : pesan
+            }
+            DataChat.push(message);
+            setPesan('');
+            console.log(DataChat);
+        }
+    }
+
+    return (
+        <div className={styles.main}>
+            <Grid className={styles.chatBox}>
+                <Grid className={styles.riwayatChat}>
+                { DataChat.map((chat, index) =>{
+                    if (chat.sent === true){
+                        return (
+                            <Paper key={index} className = { styles.sent }>{chat.body}</Paper>
+                        )
+                    } else{
+                        return (
+                            <Paper key={index} className = { styles.receive }>{chat.body}</Paper>
+                        )
+                    }
+                })}
+                </Grid>
+                <Grid className={styles.bottomChat}>
+                    <input
+                        type="text"
+                        placeholder='Pesan'
+                        className= {styles.messageBox}
+                        value= {pesan}
+                        onChange={ e => setPesan(e.target.value)}
+                    />
+                    <div className={styles.sendBox}>
+                        <Link onClick={submitPesan}>
+                            <IoIcons.IoMdSend/>
+                        </Link>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Components/ChatBox/DataChat.js
@@ -0,0 +1,19 @@
+
+export const DataChat =  [
+    {
+        sent : true,
+        body : "Hallo, Budi. Gimana kabarnya?"
+    },
+    {
+        sent : false,
+        body : "Sehat, Di. ada apa?"
+    },
+    {
+        sent : true,
+        body : "Emm anu, ini, mau pinjem duit, 2 juta doang bisa ngga?"
+    },
+    {
+        sent : false,
+        body : "Sini, Di. Besok ya!"
+    }
+];
